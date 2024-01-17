c19891f01eab3073fdb53b0c6d778ce2aeeaad52
Corofides <J.l.lendrem@smoothlines.net>
2024-01-17 21:37:27 +0000

    Create a bat; create a ball; add movement to bat and ball; allow ball to be affixed to and move with a bat

--- src/main.lua
+++ src/main.lua
@@ -9,6 +9,23 @@ import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 import "CoreLibs/timer"
 
+import "components/ball"
+import "components/bat"
+
+local pd <const> = playdate
+local gfx <const> = pd.graphics
+
+local ball <const> = Ball(60, 60)
+local bat <const> = Bat(60, 120)
+
+ball:fixToBat(bat)
+
 function playdate.update()
-   --- Main Game Loop
+
+   --- Run graphics update loop.
+   gfx.sprite.update();
+
+   --- Run timers update loop.
+   pd.timer.updateTimers();
+
 end
